150ac161bad85c8ee900ab61fa0af76c046af2bb
Kuo-Yi Chao <kuoyi.chao@hotmail.com>
2025-06-16 16:47:08 +0200

    move client to second server

--- bmw.py
+++ bmw.py
@@ -5,7 +5,7 @@ import random
 import time
 
 # === Connect to CARLA ===
-client = carla.Client('localhost', 2000)
+client = carla.Client('localhost', 3000)
 client.set_timeout(10.0)
 world = client.get_world()
 blueprint_library = world.get_blueprint_library()
